348cc45c4c76e002438c1cf912e5e3912720cf99
Robert Moga <robertmoga16@gmail.com>
2018-08-31 00:24:27 +0300

    added login method

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-
+from utils import date_time_manager as dt
+from services import profile_crawling as pc
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 if __name__== "__main__":
-    print(">> Strat")
+    CHROME_PATH = "F:\Python\InstaBot\\resources\chromedriver_win32\chromedriver.exe"
+
+    chrome_options = webdriver.ChromeOptions()
+    capabilities = {
+        'browserName': 'chrome',
+        'chromeOptions': {
+            'useAutomationExtension': False,
+            'forceDevToolsScreenshot': True,
+            'args': ['--start-maximized', '--disable-infobars']
+        }
+    }
+
+    # driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=CHROME_PATH,
+    #                           desired_capabilities=capabilities)
+
+    pc.login()
